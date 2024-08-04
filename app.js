fc4d8857c56d276373a60d9adcbb8b3c4bf1f060
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-04 11:13:15 +0200

    express.urlencoded() set extended to true. in order so that we can read values from req.body

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
-const indexRouter = require("./routes/index");
+const indexRouter = require("./routes/indexController");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: false }));
-
-app.use("/", indexRouter);
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", indexRouter);
+
+app.get("/", (req, res) => {
+  res.render("index", { message: "EJS rocks!" });
+});
 
 app.listen(PORT, () => console.log("Listening on port 3000."));
 
