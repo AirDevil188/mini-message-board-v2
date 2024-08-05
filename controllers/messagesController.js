b41ca00375916fb1c1038bde63e212d2cb177a4c
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-05 11:24:11 +0200

    added message_controller_details_get module controller

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,17 +1,21 @@
 const asyncHandler = require("express-async-handler");
+const { v4: uuidv4 } = require("uuid");
 
 const messages = [
   {
+    id: uuidv4(),
     text: "Hi, there!",
     user: "Amando",
     time: new Date(),
   },
   {
+    id: uuidv4(),
     text: "This is my first message on this app",
     user: "Jack",
     time: new Date(),
   },
   {
+    id: uuidv4(),
     text: "Howdy! How are you doing guys and girls?",
     user: "John",
     time: new Date(),
@@ -30,6 +34,7 @@ const message_controller_new_message_get = asyncHandler(
 const message_controller_new_message_post = asyncHandler(
   async (req, res, next) => {
     messages.push({
+      id: uuidv4(),
       text: req.body.text,
       user: req.body.username,
       time: new Date(),
@@ -38,8 +43,14 @@ const message_controller_new_message_post = asyncHandler(
   }
 );
 
+const message_controller_details_get = asyncHandler(async (req, res, next) => {
+  const message = messages.find((message) => (message.id = req.params.id));
+  res.render("message-detail", { title: "Message detail", message: message });
+});
+
 module.exports = {
   message_controller_index_get,
   message_controller_new_message_get,
   message_controller_new_message_post,
+  message_controller_details_get,
 };
