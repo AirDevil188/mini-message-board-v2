a40afe96580cfc68ad4cdddbb8c851420483c368
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-09 09:32:50 +0200

    changed .gitignore hidden port variable in .env file

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const indexRouter = require("./routes/index");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
-app.listen(PORT, () => console.log("Listening on port 3000."));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
 
 app.use((err, req, res, next) => {
   console.error(err);
