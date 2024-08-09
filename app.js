a316499bd22c4ac70851da2b960a0b3e9755be25
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-09 09:55:00 +0200

    removed port

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ dotenv.config();
 
 const indexRouter = require("./routes/index");
 
-const PORT = process.env.PORT;
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
 
 app.use((err, req, res, next) => {
   console.error(err);
