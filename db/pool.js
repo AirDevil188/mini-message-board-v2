a11921243441c2e6f674d57976197d53ba59c20d
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-08-09 09:30:25 +0200

    changed creation of pool logic

--- db/pool.js
+++ db/pool.js
@@ -4,5 +4,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = new Pool({
-  connectionString: process.env.CONNECTION_STRING,
+  connectionString:
+    process.env.STATUS === "local"
+      ? process.env.LOCAL_DB_CONNECTION_STRING
+      : process.env.EXTERNAL_DB_CONNECTION_STRING,
 });
